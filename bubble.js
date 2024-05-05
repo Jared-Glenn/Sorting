18c20f4265918d4dc5ab88a5229015cfc69bb5f9
Jared <jared_glenn@yahoo.com>
2024-05-05 02:21:02 -0600

    finish bubble sort

--- bubble.js
+++ bubble.js
@@ -1,3 +1,15 @@
-function bubbleSort() {}
+function bubbleSort(arr) {
+    for (let i = arr.length; i >= 0; i--) {
+        for (let j = 0; j < i-1; j++) {
+            if (arr[j] > arr[j+1]) {
+                let placeholder = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = placeholder;
+            }
+        }
+    }
+    console.log("Arr:", arr);
+    return arr;
+}
 
 module.exports = bubbleSort;
